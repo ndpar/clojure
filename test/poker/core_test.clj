c81973107c3548cf28658dc18f96c672ac223212
Andrey Paramonov <git@ndpar.com>
2012-08-12 11:42:36 -0400

    Refactored repeated code into hand function; fixed copy/paste error

--- test/poker/core_test.clj
+++ test/poker/core_test.clj
@@ -2,9 +2,14 @@
   (:use clojure.test
         poker.core))
 
-(let [sf (clojure.string/split "6C 7C 8C 9C TC" #" ")
-      fk (clojure.string/split "9D 9H 9S 9C 7D" #" ")
-      fh (clojure.string/split "6C 7C 8C 9C TC" #" ")]
+(defn hand
+  "Convert string to hand format"
+  [hand-string]
+  (clojure.string/split hand-string #" "))
+
+(let [sf (hand "6C 7C 8C 9C TC")
+      fk (hand "9D 9H 9S 9C 7D")
+      fh (hand "TD TC TH 7C 7D")]
 
   (deftest poker-test
     (testing "Straight flush wins over Four of a kind and Full house"
