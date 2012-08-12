3e03ee869cfa60cf9df6d95474ee982dac7684fc
Andrey Paramonov <git@ndpar.com>
2012-08-12 11:35:55 -0400

    First test cases, failing for now

--- test/poker/core_test.clj
+++ test/poker/core_test.clj
@@ -2,6 +2,14 @@
   (:use clojure.test
         poker.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(let [sf (clojure.string/split "6C 7C 8C 9C TC" #" ")
+      fk (clojure.string/split "9D 9H 9S 9C 7D" #" ")
+      fh (clojure.string/split "6C 7C 8C 9C TC" #" ")]
+
+  (deftest poker-test
+    (testing "Straight flush wins over Four of a kind and Full house"
+      (is (= sf (poker [sf fk fh]))))
+    (testing "Four of a kind wins Full house"
+      (is (= fk (poker [fk fh]))))
+    (testing "Full house wins itself"
+      (is (= fh (poker [fh fh]))))))
