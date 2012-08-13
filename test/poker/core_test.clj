788be3b77334d5c99d05f7ef15caa1fc82b9554d
Andrey Paramonov <git@ndpar.com>
2012-08-13 18:18:36 -0400

    More test cases

--- test/poker/core_test.clj
+++ test/poker/core_test.clj
@@ -10,7 +10,13 @@
 (let [sf (hand "6C 7C 8C 9C TC")
       fk (hand "9D 9H 9S 9C 7D")
       fh (hand "TD TC TH 7C 7D")
-      tp (hand "5S 5D 9H 9C 6S")]
+      fl (hand "2D 4D AD 7D 9D")
+      st (hand "6S 7S 8D 9S 5S")
+      tk (hand "2D TC 7H 7C 7D")
+      tp (hand "5S 5D 9H 9C 6S")
+      op (hand "5S 7D 9H 9C 6S")
+      jh (hand "5S 2D JH 9C 6S")
+      th (hand "5S 2D TH 9C 6S")]
 
   (deftest kind-test
     (let [fkranks (card-ranks fk)
@@ -58,7 +64,21 @@
     (testing "Four of a kind rank"
       (is (= [7 9 7] (hand-rank fk))))
     (testing "Full house rank"
-      (is (= [6 10 7] (hand-rank fh)))))
+      (is (= [6 10 7] (hand-rank fh))))
+    (testing "Flush rank"
+      (is (= [5 [14 9 7 4 2]] (hand-rank fl))))
+    (testing "Straight rank"
+      (is (= [4 9] (hand-rank st))))
+    (testing "Three of a kind rank"
+      (is (= [3 7 [10 7 7 7 2]] (hand-rank tk))))
+    (testing "Two pairs rank"
+      (is (= [2 [9 5] [9 9 6 5 5]] (hand-rank tp))))
+    (testing "One pair rank"
+      (is (= [1 9 [9 9 7 6 5]] (hand-rank op))))
+    (testing "Jack High rank"
+      (is (= [0 [11 9 6 5 2]] (hand-rank jh))))
+    (testing "Ten High rank"
+      (is (= [0 [10 9 6 5 2]] (hand-rank th)))))
 
   (deftest poker-test
     (testing "Straight flush wins over Four of a kind and Full house"
@@ -67,6 +87,8 @@
       (is (= fk (poker [fk fh]))))
     (testing "Full house wins itself"
       (is (= fh (poker [fh fh]))))
+    (testing "Jack High wins Ten High"
+      (is (= jh (poker [jh th]))))
     (testing "Extreme case: Single hand is allowed in poker function"
       (is (= sf (poker [sf]))))
     (testing "Extreme case: 100 hands are allowed in poker function"
