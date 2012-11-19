6289a6e169463855f3da67ea48328938d20ebade
Andrey Paramonov <git@ndpar.com>
2012-11-19 11:13:46 -0500

    Replaced -handler functions with anonymous ones

--- src/dojo/handler.clj
+++ src/dojo/handler.clj
@@ -1,22 +1,20 @@
 (ns dojo.handler)
 
 (defmacro handler [name args & body]
-  `(defn- ~(symbol (str name "-handler")) [~'msg]
+  `(fn [~'msg]
      (let [~@(interleave args (map (fn [x] `(get ~'msg ~(keyword x))) args))]
        (when (or ~@args)
          ~@body))))
 
-(handler doc [a c]
-  (format "Document:%s:%s" a c))
-
-(handler note [b c]
-  (format "Note:%s:%s" b c))
-
-(handler alert [a b]
-  (format "Alert:%s:%s" a b))
-
 (defn- handlers []
-  [doc-handler note-handler alert-handler])
+  [(handler doc [a c]
+     (format "Document:%s:%s" a c))
+
+   (handler note [b c]
+     (format "Note:%s:%s" b c))
+
+   (handler alert [a b]
+     (format "Alert:%s:%s" a b))])
 
 (defn on-message [msg]
   (letfn [(handle [acc h]
