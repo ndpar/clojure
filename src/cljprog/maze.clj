85cc30eb77dacbc93b1c6d89cbc1b595466c8671
Andrey Paramonov <github@ndpar.com>
2016-06-19 10:10:24 -0400

    hex-grid

--- src/cljprog/maze.clj
+++ src/cljprog/maze.clj
@@ -50,5 +50,55 @@
 
 ; (ns cljprog.maze)
 ; (load "maze")
+
 ; (draw 3 3 #{#{[1 0] [1 1]} #{[2 1] [1 1]} #{[0 0] [0 1]} #{[1 1] [1 2]}})
 ; (draw 40 40 (maze (grid 40 40)))
+
+(defn hex-grid
+  [w h]
+  (let [vertices (set (for [y (range h) x (range (if (odd? y) 1 0) (* 2 w) 2)] ; x is a function of y
+                        [x y]))
+        deltas [[2 0] [1 1] [-1 1]]]
+    (set (for [v vertices d deltas f [+ -]
+               :let [w (vertices (map f v d))]
+               :when w] #{v w}))))
+
+(defn- hex-outer-walls
+  [w h]
+  (let [vertices (set (for [y (range h) x (range (if (odd? y) 1 0) (* 2 w) 2)]
+                        [x y]))
+        deltas [[2 0] [1 1] [-1 1]]]
+    (set (for [v vertices d deltas f [+ -]
+               :let [w (map f v d)]
+               :when (not (vertices w))] #{v (vec w)}))))
+
+(defn hex-draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+      (doto (proxy [javax.swing.JPanel] []
+              (paintComponent [^java.awt.Graphics g]
+                (let [maze (into maze (hex-outer-walls w h))
+                      g (doto ^java.awt.Graphics2D (.create g)
+                          (.scale 10 10)
+                          (.translate 1.5 1.5)
+                          (.setStroke (java.awt.BasicStroke. 0.4
+                                                             java.awt.BasicStroke/CAP_ROUND
+                                                             java.awt.BasicStroke/JOIN_MITER)))
+                      draw-line (fn [[[xa ya] [xb yb]]]
+                                  (.draw g
+                                         (java.awt.geom.Line2D$Double.
+                                           xa (* 2 ya) xb (* 2 yb))))]
+                  (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                    (draw-line
+                      (cond
+                        (= ya yb) [[(inc xa) (+ ya 0.4)] [(inc xa) (- ya 0.4)]]
+                        (< ya yb) [[(inc xa) (+ ya 0.4)] [xa (+ ya 0.6)]]
+                        :else [[(inc xa) (- ya 0.4)] [xa (- ya 0.6)]]))))))
+        (.setPreferredSize (java.awt.Dimension.
+                             (* 20 (inc w)) (* 20 (+ 0.5 h))))))
+    .pack
+    (.setVisible true)))
+
+; (hex-draw 3 3 (hex-grid 3 3))
+; (hex-draw 20 20 (maze (hex-grid 20 20)))
